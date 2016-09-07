b13dc761951c0ec5c7a4a3aeff5492ae98d996ed
Alvin Ng and Noah Ramey <alvin@>
2016-09-07 09:51:25 -0700

    add previous method to lesson model, and add error catching to next method on lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -8,6 +8,20 @@ class Lesson < ActiveRecord::Base
   def next
     current_num = self.number
     new_lesson = Lesson.find_by(number: current_num+1, section_id: self.section_id)
-    new_lesson
+    if new_lesson
+      new_lesson
+    else
+      "This course is currently not available."
+    end
+  end
+
+  def previous
+    current_num = self.number
+    new_lesson = Lesson.find_by(number: current_num-1, section_id: self.section_id)
+    if new_lesson
+      new_lesson
+    else
+      "This course is currently not available."
+    end
   end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -14,5 +14,27 @@ describe Lesson do
       next_lesson = Lesson.create(name: 'lesson2', content: 'example2' ,number: 2, section_id: section.id)
       expect(current_lesson.next).to eq next_lesson
     end
+    it 'returns an error if there is no upcoming lesson.' do
+      course = Course.create(name: "Rails")
+      section = Section.create(name: "Week 1", course_id: course.id)
+      current_lesson = Lesson.create(name: 'lesson1', content: 'example1', number: 1, section_id: section.id)
+      expect(current_lesson.next).to eq "This course is currently not available."
+    end
+  end
+
+  context '#previous' do
+    it 'returns the lesson with the next number lower than the current lesson' do
+      course = Course.create(name: "Rails")
+      section = Section.create(name: "Week 1", course_id: course.id)
+      current_lesson = Lesson.create(name: 'lesson2', content: 'example2' ,number: 2, section_id: section.id)
+      previous_lesson = Lesson.create(name: 'lesson1', content: 'example1', number: 1, section_id: section.id)
+      expect(current_lesson.previous).to eq previous_lesson
+    end
+    it 'returns an error if there is no previous lesson.' do
+      course = Course.create(name: "Rails")
+      section = Section.create(name: "Week 1", course_id: course.id)
+      current_lesson = Lesson.create(name: 'lesson2', content: 'example2' ,number: 2, section_id: section.id)
+      expect(current_lesson.previous).to eq "This course is currently not available."
+    end
   end
 end
